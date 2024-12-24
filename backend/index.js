3ecc0f85c91ade4dbecdbd724351e1b4dad8741c
Yash Mankar <160096931+YashMankar1812@users.noreply.github.com>
2024-12-24 15:34:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,4 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const mongoose = require('mongoose'); // Adjust the import path if necessary
-// const mailRoutes = require('./routes/mailRoutes.js'); // Adjust the path if necessary
-// const dotenv = require('dotenv');
-// const app = express();
-// const PORT = process.env.PORT || 5000; 
 
-// // Connect to MongoDB
-// mongoose.connect( process.env.mongoDbUrl,
-// { useNewUrlParser: true, useUnifiedTopology: true })
-//  .then(() => console.log('MongoDB connected...'))
-//  .catch(err => console.log(err));
-
-  
-
-//  require('dotenv').config();
-
-
-// // Middleware
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// // Routes
-// app.use('/api/mail', mailRoutes);
-
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}`);
-// });
 
 
 // index.js
